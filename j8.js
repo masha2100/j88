76a4aed6d238f573b8fe9420de5beabd10c94bbd
Maria <ruzxkovam18@gmail.com>
2022-07-07 03:23:09 +0300

    first commit

---
+++ j8.js
@@ -0,0 +1,23 @@
+
+//1
+let marker = {
+    color: "pink",
+
+    sayCol() {
+
+    alert(this.color);
+    }
+};
+class amountInk {
+    amountInk = 0.5 ; //кол-во чернил
+}
+
+class Marker {
+    static text = "Programming";
+  }
+  
+
+
+marker.sayCol();
+let amountInk = new amountInk(0.5);
+alert( Marker.text ); 

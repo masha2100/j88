77e08200f043624996d1e1848c47b36876847ac9
Maria <ruzxkovam18@gmail.com>
2022-07-14 03:11:52 +0300

    https://github.com/masha2100/j88.git

--- j8.js
+++ j8.js
@@ -21,3 +21,28 @@ class Marker {
 marker.sayCol();
 let amountInk = new amountInk(0.5);
 alert( Marker.text ); 
+
+//2
+class Circle {
+
+    constructor(radius) {
+      
+      this.radius = radius;
+    }
+    get radius() {
+        return this._radius;
+    }
+      set radius(value) {
+        this._radius = value;
+    }
+    get diametr() {
+        return this._diametr;
+    }
+
+    area_of_a_circle(){
+        return Math.PI * this.radius * this.radius;
+    }
+    length_of_a_circle(){
+        return Math.PI * this.radius * 2;
+    }
+}
